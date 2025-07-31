ab0752be6da19eb45a0e72ef2f68de8e92929b9d
yusei-wy <31252054+yusei-wy@users.noreply.github.com>
2025-07-31 13:29:04 +0900

    第5章 原則をあえてやぶるとき, 第6章 テスト不足に気づいたら

--- money/money.go
+++ money/money.go
@@ -1,5 +1,14 @@
 package money
 
+type Money[T any] interface {
+	Amount() int
+	Times(multiplier int) T
+	Equals(other T) bool
+}
+
+var _ Money[*Dollar] = (*Dollar)(nil)
+var _ Money[*Franc] = (*Franc)(nil)
+
 type Dollar struct {
 	amount int
 }
@@ -18,4 +27,24 @@ func (d *Dollar) Times(multiplier int) *Dollar {
 
 func (d *Dollar) Equals(other *Dollar) bool {
 	return d.amount == other.amount
+}
+
+type Franc struct {
+	amount int
+}
+
+func NewFranc(amount int) *Franc {
+	return &Franc{amount: amount}
+}
+
+func (f *Franc) Amount() int {
+	return f.amount
+}
+
+func (f *Franc) Times(multiplier int) *Franc {
+	return &Franc{amount: f.amount * multiplier}
+}
+
+func (f *Franc) Equals(other *Franc) bool {
+	return f.amount == other.amount
 }
--- money/money_test.go
+++ money/money_test.go
@@ -4,6 +4,7 @@ import (
 	"tdd/money"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -16,4 +17,12 @@ func TestMultiplication(t *testing.T) {
 func TestEquality(t *testing.T) {
 	require.True(t, money.NewDollar(5).Equals(money.NewDollar(5)))
 	require.False(t, money.NewDollar(5).Equals(money.NewDollar(6)))
+	require.True(t, money.NewFranc(5).Equals(money.NewFranc(5)))
+	require.False(t, money.NewFranc(5).Equals(money.NewFranc(6)))
+}
+
+func TestFrancMultiplication(t *testing.T) {
+	five := money.NewFranc(5)
+	assert.True(t, money.NewFranc(10).Equals(five.Times(2)))
+	assert.True(t, money.NewFranc(15).Equals(five.Times(3)))
 }
